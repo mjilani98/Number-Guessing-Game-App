094218de5c54fc3e9e95502701bc58ac7af3a474
mjilani98 <mjilani@emich.edu>
2025-02-18 11:16:39 -0500

    created the main java class (Controller) Edited and added some methods to the GuessGame class(Model) added comments to GuessGame(Model)

--- app/src/main/java/com/example/hw2q5/GuessGame.java
+++ app/src/main/java/com/example/hw2q5/GuessGame.java
@@ -6,18 +6,57 @@ public class GuessGame {
 
     private int mysteryNumber;
     private int userGuess;
+    private int chances;
+    private boolean guessed;
 
+    //default constructor
     public GuessGame()
     {
         //creating a random number between 1 to 100
         Random random = new Random();
-        int mysteryNumber = random.nextInt(100) + 1;
+        mysteryNumber = random.nextInt(100) + 1;
+        //for each new game , the player has 8 chances
+        chances = 8;
     }
 
+    //method to get the mystery number
+    public int getMysteryNumber()
+    {
+        return mysteryNumber;
+    }
+
+    //method takes the user guess
     public void setUserGuess(int guess)
     {
         this.userGuess = guess;
     }
+
+    //method decrement the number of chances
+    public void decrementChances()
+    {
+        chances -= 1;
+    }
+
+    //if the user guessed the number , set the value true
+     public boolean getGuessed()
+     {
+         if(userGuess == mysteryNumber)
+         {
+             return true;
+         }
+         else
+         {
+             return false;
+         }
+     }
+
+     //method to get the number of chances left
+     public int getChances()
+     {
+         return chances;
+     }
+
+     //method returns a message based on the player guess
 
     public String getMessage()
     {
--- app/src/main/java/com/example/hw2q5/MainActivity.java
+++ app/src/main/java/com/example/hw2q5/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.hw2q5;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,11 +17,154 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GuessGame game;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        //creating a button handler object
+        ButtonHandler handler = new ButtonHandler();
+
+        //getting hold of the submit button
+        Button btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(handler);
+
+        //create a game
+        game = new GuessGame();
+
+        //setting up the screen
+        TextView txtViewChances = findViewById(R.id.txtViewNumOfGuesses);
+        txtViewChances.setText(game.getChances()+"");
+
+    }
+
+    private class ButtonHandler implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v)
+        {
+            //Number of chances left text view
+            TextView txtViewChances = findViewById(R.id.txtViewNumOfGuesses);
+
+            //response message text view
+            TextView txtViewMessage = findViewById(R.id.txtViewResponse);
+
+            //get the guess
+            EditText input = findViewById(R.id.edtTxtInput);
+            String strNumber = input.getText().toString();
+            int number =0;
+            try {
+                number = Integer.parseInt(strNumber);
+            }
+            catch (NumberFormatException e)
+            {
+                Toast toast = Toast.makeText(getApplicationContext(),"Invalid input",Toast.LENGTH_LONG);
+                toast.show();
+            }
+
+            //sending the user guess to the model
+            game.setUserGuess(number);
+
+            //if the user did not guess the number
+            if(game.getGuessed() == false)
+            {
+                //decrement the number of chances
+                game.decrementChances();
+
+
+                //show number of chances left
+                txtViewChances.setText(game.getChances()+"");
+
+                //show message of the range of numbers
+                txtViewMessage.setText(game.getMessage());
+            }
+            //if the user guessed the number
+            else
+            {
+                //disable the button so they can not keep playing
+                Button btnSubmit = findViewById(R.id.btnSubmit);
+                btnSubmit.setEnabled(false);
+                //show a message response , the user guessed the number
+                txtViewMessage.setText(game.getMessage());
+                //show a dialog box to ask if the they wish to play again
+                showDialogBox();
+            }
+
+            //if the player out of chances without guessing the number
+            if(game.getChances() == 0 )
+            {
+                //disable the button so they can not keep playing
+                Button btnSubmit = findViewById(R.id.btnSubmit);
+                btnSubmit.setEnabled(false);
+
+                //show a message that they have lost the game
+                txtViewMessage.setText("You ran out of chances\nThe number is : "+game.getMysteryNumber());
+                showDialogBox();
+            }
+        }
+    }
+
+    private void showDialogBox()
+    {
+        //create a dialog box
+        AlertDialog.Builder dialogBox = new AlertDialog.Builder(this);
+
+        //set message on dialog box
+        dialogBox.setMessage("Game ended , do you want to play again");
+
+        //create an event handler for the dialog box
+        DialogBoxListener handler = new DialogBoxListener();
+
+        //add event handler to the dialog box
+        dialogBox.setPositiveButton("Yes",handler);
+        dialogBox.setNegativeButton("No",handler);
+        dialogBox.setNeutralButton("Cancel",handler);
+        dialogBox.show();
+    }
+
+    private class DialogBoxListener implements DialogInterface.OnClickListener
+    {
+
+        //Method takes different actions depending on which button
+        //is clicked on the dialog box
+        @Override
+        public void onClick(DialogInterface dialog, int id)
+        {
+            //if the positive button is clicked , replay the game
+            if(id == -1)
+            {
+                //creating a new game
+                game = new GuessGame();
+                //resetting the number of chances
+                TextView txtViewChances = findViewById(R.id.txtViewNumOfGuesses);
+                txtViewChances.setText(game.getChances()+"");
+
+                //resetting the response message
+                TextView txtViewMessage = findViewById(R.id.txtViewResponse);
+                txtViewMessage.setText("");
+
+                //reset the input box
+                EditText input = findViewById(R.id.edtTxtInput);
+                input.setText("");
+
+                //enable the button
+                Button btnSubmit = findViewById(R.id.btnSubmit);
+                btnSubmit.setEnabled(true);
+
+
+            }
+            //if negative button is clicked , end the game and end the app
+            else if(id == -2)
+            {
+                //destroy the app
+                MainActivity.this.finish();
+            }
+            //if neutral button is clicked, dialog box disappear
+            else;
+        }
     }
 }
